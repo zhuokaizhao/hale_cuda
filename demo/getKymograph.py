2b930a22e4580f3d41c3b67e9b22c3f1b4c633cc
zhuokaizhao <zhuokai@uchicago.edu>
2019-04-16 14:35:00 -0500

    get some results

--- demo/getKymograph.py
+++ demo/getKymograph.py
@@ -62,6 +62,14 @@ def addGaussianNoise(loc, sigma):
     
     return loc_noisy
 
+# Helper function that computes the root mean squared error between two sets of curvatures
+def computeRMS(cur1, cur2):
+    rms = np.sqrt(np.sum((cur1 - cur2) ** 2))
+    # we assume that two images have the same dimensions
+    rms /= float(cur1.size)
+
+    return rms
+
 # helper function that performs 3D scatter plot with annotation
 def plot3D(i, time, x, y, z, myTitle):
     fig = plt.figure(i)
@@ -79,7 +87,9 @@ def plot3D(i, time, x, y, z, myTitle):
 
 def main(argv):
     print_help()
-    filename = argv[0] if len(argv) > 1 else 'coord_newtrack_pioneer_scale_old.txt'
+    filename = argv[0] if len(argv) > 0 else 'coord_newtrack_pioneer_scale_old.txt'
+    timeShift = float(argv[1]) if len(argv) > 1 else 5
+    sigma = float(argv[2]) if len(argv) > 2 else 1
     
     # load the txt file
     with open(filename) as textFile:
@@ -100,10 +110,10 @@ def main(argv):
     # make synthetic followers data
     allPositions_follower = allPositions_pioneer
     # all time stamp plus 10
-    time_follower = allPositions_follower[:,0] + 10
-    x_follower = allPositions_follower[:,1] + addGaussianNoise(x_pioneer, 2)
-    y_follower = allPositions_follower[:,2] + addGaussianNoise(y_pioneer, 3)
-    z_follower = allPositions_follower[:,3] + addGaussianNoise(z_pioneer, 1)
+    time_follower = allPositions_follower[:,0] + timeShift
+    x_follower = allPositions_follower[:,1] + addGaussianNoise(x_pioneer, sigma)
+    y_follower = allPositions_follower[:,2] + addGaussianNoise(y_pioneer, sigma)
+    z_follower = allPositions_follower[:,3] + addGaussianNoise(z_pioneer, sigma)
 
     # 3D scatter plot of followers
     plot3D(2, time_follower, x_follower, y_follower, z_follower, 'Follower locations vs t')
@@ -118,10 +128,46 @@ def main(argv):
     plt.plot(time_pioneer, curvature_pioneer)
     plt.plot(time_follower, curvature_follower)
     plt.legend(('Pioneer', 'Follower'))
-    plt.title('Pioneer(real) and follower(synthetic) neurons positions vs time')
+    myTitle = 'Pioneer(real) and follower(synthetic) neurons positions vs time\nRMS =' + str(computeRMS(curvature_pioneer, curvature_follower))
+    plt.title(myTitle)
     plt.xlabel('t')
     plt.ylabel('Curvature')
+
+    # generate the RMS plot as noise level goes up
+    allRMS = []
+    allSigma = []
+    for i in range(10000):
+        sigma = sigma + 0.01
+        allSigma.append(sigma)
+        curSumRMS = 0
+        for j in range(10):
+            # make synthetic followers data
+            allPositions_follower = allPositions_pioneer
+            # all time stamp plus 10
+            time_follower = allPositions_follower[:,0] + timeShift
+            x_follower = allPositions_follower[:,1] + addGaussianNoise(x_pioneer, sigma)
+            y_follower = allPositions_follower[:,2] + addGaussianNoise(y_pioneer, sigma)
+            z_follower = allPositions_follower[:,3] + addGaussianNoise(z_pioneer, sigma)
+
+            curvature_follower = computeCurvature(x_follower, y_follower, z_follower)
+            curSumRMS = curSumRMS + computeRMS(curvature_pioneer, curvature_follower)
+        
+        # get the average of sum
+        curRMS = float(curSumRMS/10.)
+        allRMS.append(curRMS)
+
+    # plot the RMS vs sigma
+    plt.figure(4)
+    plt.plot(allSigma, allRMS)
+    myTitle = 'Root squared mean error as sigma increases'
+    plt.title(myTitle)
+    plt.xlabel('Sigma')
+    plt.ylabel('RMS')
+    
+    
     plt.show()
+
+
 
 
 if __name__ == "__main__":

5454d7eca4e8b16df5b8a3dcb3961b8675f62fe7
zhuokaizhao <zhuokai@uchicago.edu>
2019-04-22 15:43:40 -0500

    added synthetic dataset

--- demo/getKymograph.py
+++ demo/getKymograph.py
@@ -13,7 +13,7 @@ def print_help():
     This program generates kymograph based on tracked pioneer neuron locations
     
     Usage:
-    python/pythonw getKymograph.py [tracked_file -- default coord_newtrack_pioneer_scale_old.txt]''')
+    python/pythonw getKymograph.py [mode -- synthetic or real, default synthetic] [tracked_file -- default coord_newtrack_pioneer_scale_old.txt]''')
 
 # helper fucntion that computes curvatures
 def computeCurvature(x, y, z):
@@ -87,85 +87,118 @@ def plot3D(i, time, x, y, z, myTitle):
 
 def main(argv):
     print_help()
-    filename = argv[0] if len(argv) > 0 else 'coord_newtrack_pioneer_scale_old.txt'
-    timeShift = float(argv[1]) if len(argv) > 1 else 5
-    sigma = float(argv[2]) if len(argv) > 2 else 1
-    
-    # load the txt file
-    with open(filename) as textFile:
-        allPositions_pioneer = [line.split() for line in textFile]
+    mode = argv[0] if len(argv) > 0 else 'synthetic'
+    filename = argv[1] if len(argv) > 1 else 'coord_newtrack_pioneer_scale_old.txt'
+    timeShift = float(argv[2]) if len(argv) > 2 else 5
+    sigma = float(argv[3]) if len(argv) > 3 else 1
 
-    # we right now only have pioneer data
-    allPositions_pioneer = np.asarray(allPositions_pioneer)
-    # change from string to float
-    allPositions_pioneer = allPositions_pioneer.astype(float)
-    time_pioneer = allPositions_pioneer[:,0]
-    x_pioneer = allPositions_pioneer[:,1]
-    y_pioneer = allPositions_pioneer[:,2]
-    z_pioneer = allPositions_pioneer[:,3]
+    # synthetic helix dataset
+    if mode == 'synthetic':
+        # number of data points that we are generating
+        n = 1000
 
-    # 3D scatter plot of pioneers
-    plot3D(1, time_pioneer, x_pioneer, y_pioneer, z_pioneer, 'Pioneer locations vs t')
+        # Plot a right-hand helix along the z-axis
+        theta_max = 8 * np.pi
+        theta = np.linspace(0, theta_max, n)
+        x =  np.cos(theta)
+        y =  np.sin(theta)
+        z = theta
 
-    # make synthetic followers data
-    allPositions_follower = allPositions_pioneer
-    # all time stamp plus 10
-    time_follower = allPositions_follower[:,0] + timeShift
-    x_follower = allPositions_follower[:,1] + addGaussianNoise(x_pioneer, sigma)
-    y_follower = allPositions_follower[:,2] + addGaussianNoise(y_pioneer, sigma)
-    z_follower = allPositions_follower[:,3] + addGaussianNoise(z_pioneer, sigma)
+        # make the plot
+        fig = plt.figure(1)
+        ax1 = fig.add_subplot(121, projection='3d')
+        ax1.plot(x, y, z, 'b', lw=2)
+        # An line through the centre of the helix
+        ax1.plot((0,0), (0,0), (-theta_max*0.2, theta_max * 1.2), color='k', lw=2)
 
-    # 3D scatter plot of followers
-    plot3D(2, time_follower, x_follower, y_follower, z_follower, 'Follower locations vs t')
-    
+        # get the curvature of both pioneer and follower
+        curvature = computeCurvature(x, y, z)
 
-    # get the curvature of both pioneer and follower
-    curvature_pioneer = computeCurvature(x_pioneer, y_pioneer, z_pioneer)
-    curvature_follower = computeCurvature(x_follower, y_follower, z_follower)
-
-    # generate kymograph
-    plt.figure(3)
-    plt.plot(time_pioneer, curvature_pioneer)
-    plt.plot(time_follower, curvature_follower)
-    plt.legend(('Pioneer', 'Follower'))
-    myTitle = 'Pioneer(real) and follower(synthetic) neurons positions vs time\nRMS =' + str(computeRMS(curvature_pioneer, curvature_follower))
-    plt.title(myTitle)
-    plt.xlabel('t')
-    plt.ylabel('Curvature')
-
-    # generate the RMS plot as noise level goes up
-    allRMS = []
-    allSigma = []
-    for i in range(10000):
-        sigma = sigma + 0.01
-        allSigma.append(sigma)
-        curSumRMS = 0
-        for j in range(10):
-            # make synthetic followers data
-            allPositions_follower = allPositions_pioneer
-            # all time stamp plus 10
-            time_follower = allPositions_follower[:,0] + timeShift
-            x_follower = allPositions_follower[:,1] + addGaussianNoise(x_pioneer, sigma)
-            y_follower = allPositions_follower[:,2] + addGaussianNoise(y_pioneer, sigma)
-            z_follower = allPositions_follower[:,3] + addGaussianNoise(z_pioneer, sigma)
-
-            curvature_follower = computeCurvature(x_follower, y_follower, z_follower)
-            curSumRMS = curSumRMS + computeRMS(curvature_pioneer, curvature_follower)
+        # generate kymograph
+        ax2 = fig.add_subplot(122)
+        ax2.plot(theta, curvature)
+        myTitle = 'Synthetic helix curvature vs time'
+        plt.title(myTitle)
+        plt.xlabel('theta')
+        plt.ylabel('Curvature')
         
-        # get the average of sum
-        curRMS = float(curSumRMS/10.)
-        allRMS.append(curRMS)
+        plt.show()
 
-    # plot the RMS vs sigma
-    plt.figure(4)
-    plt.plot(allSigma, allRMS)
-    myTitle = 'Root squared mean error as sigma increases'
-    plt.title(myTitle)
-    plt.xlabel('Sigma')
-    plt.ylabel('RMS')
-    
-    
-    plt.show()
+    # real data mode
+    elif mode == 'real':
+        # load the txt file
+        with open(filename) as textFile:
+            allPositions_pioneer = [line.split() for line in textFile]
+
+        # we right now only have pioneer data
+        allPositions_pioneer = np.asarray(allPositions_pioneer)
+        # change from string to float
+        allPositions_pioneer = allPositions_pioneer.astype(float)
+        time_pioneer = allPositions_pioneer[:,0]
+        x_pioneer = allPositions_pioneer[:,1]
+        y_pioneer = allPositions_pioneer[:,2]
+        z_pioneer = allPositions_pioneer[:,3]
+
+        # 3D scatter plot of pioneers
+        plot3D(1, time_pioneer, x_pioneer, y_pioneer, z_pioneer, 'Pioneer locations vs t')
+
+        # make synthetic followers data
+        allPositions_follower = allPositions_pioneer
+        # all time stamp plus 10
+        time_follower = allPositions_follower[:,0] + timeShift
+        x_follower = allPositions_follower[:,1] + addGaussianNoise(x_pioneer, sigma)
+        y_follower = allPositions_follower[:,2] + addGaussianNoise(y_pioneer, sigma)
+        z_follower = allPositions_follower[:,3] + addGaussianNoise(z_pioneer, sigma)
+
+        # 3D scatter plot of followers
+        plot3D(2, time_follower, x_follower, y_follower, z_follower, 'Follower locations vs t')
+        
+
+        # get the curvature of both pioneer and follower
+        curvature_pioneer = computeCurvature(x_pioneer, y_pioneer, z_pioneer)
+        curvature_follower = computeCurvature(x_follower, y_follower, z_follower)
+
+        # generate kymograph
+        plt.figure(3)
+        plt.plot(time_pioneer, curvature_pioneer)
+        plt.plot(time_follower, curvature_follower)
+        plt.legend(('Pioneer', 'Follower'))
+        myTitle = 'Pioneer(real) and follower(synthetic) neurons positions vs time\nSigma=' + str(sigma) + '\nRMS =' + str(computeRMS(curvature_pioneer, curvature_follower))
+        plt.title(myTitle)
+        plt.xlabel('t')
+        plt.ylabel('Curvature')
+
+        # generate the RMS plot as noise level goes up
+        allRMS = []
+        allSigma = []
+        for i in range(10000):
+            sigma = sigma + 0.01
+            allSigma.append(sigma)
+            curSumRMS = 0
+            for j in range(10):
+                # make synthetic followers data
+                allPositions_follower = allPositions_pioneer
+                # all time stamp plus 10
+                time_follower = allPositions_follower[:,0] + timeShift
+                x_follower = allPositions_follower[:,1] + addGaussianNoise(x_pioneer, sigma)
+                y_follower = allPositions_follower[:,2] + addGaussianNoise(y_pioneer, sigma)
+                z_follower = allPositions_follower[:,3] + addGaussianNoise(z_pioneer, sigma)
+
+                curvature_follower = computeCurvature(x_follower, y_follower, z_follower)
+                curSumRMS = curSumRMS + computeRMS(curvature_pioneer, curvature_follower)
+            
+            # get the average of sum
+            curRMS = float(curSumRMS/10.)
+            allRMS.append(curRMS)
+
+        # plot the RMS vs sigma
+        plt.figure(4)
+        plt.plot(allSigma, allRMS)
+        myTitle = 'Root squared mean error as sigma increases'
+        plt.title(myTitle)
+        plt.xlabel('Sigma')
+        plt.ylabel('RMS')
+        plt.show()
 
 
 
